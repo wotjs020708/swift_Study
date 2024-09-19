996ccd40d29dfd099081f3fac237fd2c69c51af7
JaeSunEo <wotjs020708@gmail.com>
2024-09-19 21:02:55 +0900

    Add Snapkit, label을 활용하여 간단한 snapKit과 기존 코드 차이 확인

--- useSnapKit/useSnapKit/ViewController.swift
+++ useSnapKit/useSnapKit/ViewController.swift
@@ -6,14 +6,42 @@
 //
 
 import UIKit
-
+import SnapKit
 class ViewController: UIViewController {
-
+    
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .red
+        
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
+        
     }
-
-
+    
+    
+    func setupUI() {
+        self.view.backgroundColor = .blue
+        self.view.addSubview(label)
+        
+        label.snp.makeConstraints { make in
+            make.width.height.equalTo(100)
+            make.center.equalToSuperview()
+            
+        }
+        
+//        NSLayoutConstraint.activate([
+//            label.centerXAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerXAnchor),
+//            label.centerYAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.centerYAnchor),
+//            label.heightAnchor.constraint(equalToConstant: 100),
+//            label.widthAnchor.constraint(equalToConstant: 100),
+//        ])
+    }
+    
 }
 
