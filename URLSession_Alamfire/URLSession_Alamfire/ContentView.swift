ff5c8de4cb31dad270727dfc6f24611cc49b6f4a
JaeSunEo <wotjs020708@gmail.com>
2024-09-26 22:36:59 +0900

    Initialize project URLSecssion_Alamfire

---
+++ URLSession_Alamfire/URLSession_Alamfire/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  URLSession_Alamfire
+//
+//  Created by 어재선 on 9/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ URLSession_Alamfire/URLSession_Alamfire/URLSession_AlamfireApp.swift
@@ -0,0 +1,17 @@
+//
+//  URLSession_AlamfireApp.swift
+//  URLSession_Alamfire
+//
+//  Created by 어재선 on 9/26/24.
+//
+
+import SwiftUI
+
+@main
+struct URLSession_AlamfireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
