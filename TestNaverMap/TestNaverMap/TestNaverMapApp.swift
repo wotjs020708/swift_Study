77a00154d96b1a8325ab0e4a364a4decf0f6cd7b
JaeSunEo <wotjs020708@gmail.com>
2025-04-10 14:49:57 +0900

    add TestNaverMapApp Project initialize

---
+++ TestNaverMap/TestNaverMap/TestNaverMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestNaverMapApp.swift
+//  TestNaverMap
+//
+//  Created by 어재선 on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TestNaverMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
