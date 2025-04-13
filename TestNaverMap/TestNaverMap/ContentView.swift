1f7efdf11983d9403b71b66224ce4562a6c1df6c
JaeSunEo <wotjs020708@gmail.com>
2025-04-13 12:39:05 +0900

    [feat] 네이버 지도 구현

--- TestNaverMap/TestNaverMap/ContentView.swift
+++ TestNaverMap/TestNaverMap/ContentView.swift
@@ -6,19 +6,35 @@
 //
 
 import SwiftUI
-
+import NMapsMap
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            UIMapView()
+                .edgesIgnoringSafeArea(.vertical)
         }
         .padding()
     }
 }
 
+
+struct UIMapView: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> UIView {
+        let view = NMFNaverMapView()
+        view.showZoomControls = false
+        view.mapView.positionMode = .direction
+        view.mapView.zoomLevel = 17
+        
+        return view
+    }
+    func updateUIView(_ uiView: UIView, context: Context) {
+        
+    }
+    
+    
+    
+}
 #Preview {
     ContentView()
 }
