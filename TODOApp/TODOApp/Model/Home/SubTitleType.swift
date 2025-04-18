a92217f42902798809f0b06fb30b94a22618a091
JaeSunEo <wotjs020708@gmail.com>
2025-04-18 18:05:40 +0900

    add SubTitleType

---
+++ TODOApp/TODOApp/Model/Home/SubTitleType.swift
@@ -0,0 +1,14 @@
+//
+//  SubTitleType.swift
+//  TODOApp
+//
+//  Created by 어재선 on 4/18/25.
+//
+
+import Foundation
+
+enum SubTitleType: String {
+    case todo = "Todo"
+    case done = "Done"
+    case inProgress = "In Progress"
+}
