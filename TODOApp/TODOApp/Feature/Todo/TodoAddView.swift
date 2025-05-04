1262e7e6dced8fcd03ee2fb6203345e78f63e325
JaeSunEo <wotjs020708@gmail.com>
2025-05-04 22:10:53 +0900

    [feat] addTodoView

--- TODOApp/TODOApp/Feature/Todo/TodoAddView.swift
+++ TODOApp/TODOApp/Feature/Todo/TodoAddView.swift
@@ -6,11 +6,139 @@
 //
 
 import SwiftUI
-
+import SwiftData
 struct TodoAddView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Environment(\.dismiss) private var dismiss
+    @State var title: String = ""
+    @State var note: String = ""
+    @State var startDate: Date = .now
+    @State var endDate: Date = .now
+    @State var isShowStartDatePicker: Bool = false
+    @State var isShowEndDatePicker: Bool = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        CustomNavigationBar(
+            isDisplayLeftBtn: true,
+            titleText: "ADD TODO",
+            leftBtnAction: {
+              dismiss()
+            },
+            rightBtnAction: {
+                addTodo(todo: Todo(title: title, startDate: startDate), modelContext: modelContext)
+                dismiss()
+                
+            }
+            
+            
+            
+        )
+        ScrollView {
+            VStack {
+                
+                subTitleView(text: "Title")
+                TitleTextField(text: $title)
+                Spacer()
+                    .frame(height: 16)
+                subTitleView(text: "Date")
+                customPickerView(text: "StartDate", date: $startDate, isShowDatePicker: isShowStartDatePicker)
+                Spacer()
+                    .frame(height: 8)
+                customPickerView(text: "EndDate", date: $endDate, isShowDatePicker: isShowEndDatePicker)
+                Spacer()
+                    .frame(height: 16)
+                subTitleView(text: "Note")
+                Spacer()
+                    .frame(height: 8)
+                NoteTextField(text: $note)
+                Spacer()
+                
+            }
+            .padding(.horizontal, 16)
+        }
+        .navigationBarBackButtonHidden(true)
     }
+
+}
+//MARK: - TitleTextField
+private struct TitleTextField: View {
+    var text: Binding<String>
+    fileprivate var body: some View {
+        TextField("Text", text: text)
+            .padding(16)
+            .font(.system(size: 24, weight: .medium))
+            .frame(width: .infinity, height: 56)
+            .background(Color.gray.opacity(0.2))
+            .cornerRadius(16)
+    }
+}
+//MARK: - NoteTextField
+private struct NoteTextField: View {
+    var text: Binding<String>
+    fileprivate var body: some View {
+        TextEditor(text: text)
+            
+            .scrollContentBackground(.hidden)
+            .padding(16)
+            .background(.gray.opacity(0.2))
+            .cornerRadius(16)
+            .frame(width: .infinity, height: 500)
+            
+        
+    }
+}
+//MARK: - subTitleView
+private struct subTitleView: View {
+    var text: String
+    fileprivate var body: some View {
+        HStack{
+            Text(text)
+                .font(.system(size: 16, weight: .bold))
+                .foregroundStyle(Color(.lightGray))
+            Spacer()
+        }
+        
+    }
+}
+
+//MARK: - customPickerView
+private struct customPickerView: View {
+    var text: String
+    var date: Binding<Date>
+    @State var isShowDatePicker: Bool
+    fileprivate var body: some View {
+        Button(
+            action: {
+                isShowDatePicker.toggle()
+                
+            },
+            label: {
+                Text(text)
+                    .font(.system(size: 24, weight: .medium))
+                    .frame(width: 370, height: 56)
+                    .background(.gray.opacity(0.2))
+                    .foregroundStyle(.gray.opacity(0.2))
+                    .cornerRadius(16)
+            }
+            
+        )
+        if isShowDatePicker {
+            DatePicker(
+                                "",
+                                selection: date,
+                                displayedComponents: .date
+                            )
+                            .labelsHidden()
+                            .datePickerStyle(.graphical)
+                            .frame(maxHeight: 400)
+
+        }
+    }
+}
+
+// MARK: - function
+private func addTodo(todo: Todo, modelContext: ModelContext) {
+        modelContext.insert(todo)
 }
 
 #Preview {
