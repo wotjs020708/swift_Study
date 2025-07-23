188bbe7a5eda74a73f4229edd96ead8ca23b6ef8
JaeSunEo <wotjs020708@gmail.com>
2025-07-23 16:11:29 +0900

    add TCA_Simple_tutorial Project

---
+++ TCA_Simple_tutorial/TCA_Simple_tutorial/TCA_Simple_tutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_Simple_tutorialApp.swift
+//  TCA_Simple_tutorial
+//
+//  Created by 어재선 on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct TCA_Simple_tutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CounterView()
+        }
+    }
+}
