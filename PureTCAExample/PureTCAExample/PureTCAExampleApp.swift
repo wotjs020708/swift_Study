2ccc4080842f468d99c24a8805009f12ec47de6d
JaeSunEo <wotjs020708@gmail.com>
2025-08-05 14:47:44 +0900

    Initialize Project PureTCAExample

---
+++ PureTCAExample/PureTCAExample/PureTCAExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PureTCAExampleApp.swift
+//  PureTCAExample
+//
+//  Created by 어재선 on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct PureTCAExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
