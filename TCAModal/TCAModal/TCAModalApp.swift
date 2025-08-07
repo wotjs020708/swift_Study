232c50bf025b2012581a10e4f1d2094531b1a01a
JaeSunEo <wotjs020708@gmail.com>
2025-08-07 14:14:05 +0900

    initailze Project

---
+++ TCAModal/TCAModal/TCAModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCAModalApp.swift
+//  TCAModal
+//
+//  Created by 어재선 on 8/7/25.
+//
+
+import SwiftUI
+
+@main
+struct TCAModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
